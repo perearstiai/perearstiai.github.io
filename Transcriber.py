ed0a7f6f4dde21edd4145393c22127974413de28
Karl-Johan Pilve <pilve@ut.ee>
2024-01-04 12:52:29 +0200

    Correct transcriber file

--- Transcriber.py
+++ Transcriber.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 
 
-class Transcriber():
+class WhisperAPI:
     def __init__(self, model) -> None:
         self.client = OpenAI()
         self.model = model
@@ -11,9 +11,7 @@ class Transcriber():
         af = open(audio_file, "rb")
 
         transcription = self.client.audio.transcriptions.create(
-            model=self.model,
-            file=af,
-            response_format=self.response_format
+            model=self.model, file=af, response_format=self.response_format
         )
 
         af.close()
