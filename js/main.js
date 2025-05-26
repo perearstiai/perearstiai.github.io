5149bb099089b2415632671d9a3f3af0a03ad475
rauno <raunoraa13@gmail.com>
2025-05-26 16:27:02 +0300

    Removed TODO comments, made them issues instead.

--- js/main.js
+++ js/main.js
@@ -4,21 +4,6 @@ import { setupSummarizer } from './summarizer.js';
 import { setupSettingsModal, requireSettings, requireLang } from './settings.js';
 import { setupFooterLinks } from './footer-links.js';
 
-// TODO
-/*
-1. Make language selection dropdown prettier.
-2. Add loading spinners to corresponding places when waiting for corresponding API responses.
-3. Disable later sections if no input is provided in previous sections.
-    Also provide info messages why something is disabled (e.g. on hovering the disabled component).
-4. Refactor and clean up code (including HTML and CSS).
-5. Add options to settings to select different transcription and summarization models.
-6. Make certain page components more responsive.
-7. Right now, it is briefly seen when js is gradually applied as dom is loaded, 
-    but it would be better to hide the main content until everything is ready (use loading screen with spinner instead).
-8. Animate the settings button on hover (e.g., rotate it).
-9. Do something about using (text) file of transcription as input for summarization.
-*/
-
 window.addEventListener('DOMContentLoaded', async () => {
   await requireLang();
   setupSettingsModal();
